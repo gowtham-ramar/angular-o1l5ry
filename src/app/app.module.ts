0d992afdf571b83c766e03cb9586e5b56915ce81
gowtham-ramar <34623734+gowtham-ramar@users.noreply.github.com>
2022-11-04 07:25:52 +0530

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,15 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { Day2Module } from './day2/day2.module';
 import { AppRoutingModule } from './app.routing.module';
+import { ReactiveComponent } from './day5/component/reactive/reactive.component';
+import { TemplateComponent } from './day5/component/template/template.component';
+import { MustMatchDirective } from './day5/component/must-match.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,      
-    Day2Module.forRoot(),
-    AppRoutingModule,
-    
-  ],
-  exports: [
-    
-  ],
+  declarations: [AppComponent, ReactiveComponent, TemplateComponent,MustMatchDirective],
+  imports: [BrowserModule, FormsModule, Day2Module.forRoot(), AppRoutingModule],
+  exports: [],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
